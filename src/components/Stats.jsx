d8b00285928585a06c3425433e4e406c29fb1216
sophiepavlova <olni.pavlova@gmail.com>
2026-04-24 10:03:16 -0700

    Style: finish changes in ui

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,16 @@
+import { PieChart, Pie, Cell } from 'recharts';
 export default function Stats() {
+  const data = [
+    { name: 'Done', value: 6 },
+    { name: 'Remaining', value: 3 },
+  ];
+  const COLORS = ['#ee861e', '#5d6d91'];
+
   return (
     <div>
       <h1>Statistics</h1>
 
-      <div>
+      <div className='stats-messages'>
         <div className='stats-message'>
           <div className='number done'>6</div>
           <div>things done today!</div>
@@ -15,17 +22,25 @@ export default function Stats() {
       </div>
 
       <div className='diagram'>
-        Diagram
-        <div className='percents'>
-          <div className='cirlce done'></div>
-          <div>66 %</div>
+        <PieChart width={200} height={200}>
+          <Pie data={data} dataKey='value' innerRadius={60} outerRadius={80}>
+            {data.map((entry, index) => (
+              <Cell key={index} fill={COLORS[index]} />
+            ))}
+          </Pie>
+        </PieChart>
+        <div>
+          <div className='percents'>
+            <div className='cirlce done'></div>
+            <div>66 %</div>
+          </div>
+          <div className='percents'>
+            <div className='cirlce not-done'></div>
+            <div>33 %</div>
+          </div>
         </div>
-        <div className='percents'>
-          <div className='cirlce not-done'></div>
-          <div>33 %</div>
-        </div>
+        <div className='fraction'>6/9</div>
       </div>
-      <div>6/9</div>
     </div>
   );
 }
